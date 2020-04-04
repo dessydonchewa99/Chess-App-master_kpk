f70ecd160a406059f74e939549d003c3801a849b
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-04 19:04:37 +0300

    changed parameter names

--- ChessGamePracticumPROJECT/Move.h
+++ ChessGamePracticumPROJECT/Move.h
@@ -2,26 +2,27 @@
 #include"Position.h"
 class Move
 {
+private:
 	Position* from;
 	Position* to;
 	bool isAttacking;
 public:
 	int getFromRow()const;
 	int getToRow()const;
-	int getFromCol()const;
-	int getToCol()const;
+	int getFromColumn()const;
+	int getToColumn()const;
 	bool getIsAttacking()const;
 
 	void setFromRow(int row);
 	void setToRow(int row);
-	void setFromCol(int col);
-	void setToCol(int col);
+	void setFromColumn(int column);
+	void setToColumn(int column);
 	void setIsAttacking(bool isAttacking);
 
 	void logToConsole();
 
 	Move();
-	Move(int rowFrom, int colFrom, int rowTo, int colTo, bool isAttacking = false);
-	 ~Move();
+	Move(int rowFrom, int columnFrom, int rowTo, int columnTo, bool isAttacking = false);
+	~Move();
 };
 
