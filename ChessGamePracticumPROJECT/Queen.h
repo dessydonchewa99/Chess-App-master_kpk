70218e3994b35ecb23f42423621a95968f965376
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-05 17:06:47 +0300

    formatted

--- ChessGamePracticumPROJECT/Queen.h
+++ ChessGamePracticumPROJECT/Queen.h
@@ -2,18 +2,17 @@
 #include "Figure.h"
 #include"Bishop.h"
 #include"Rook.h"
-class Queen :
-	public Rook, public Bishop
+
+class Queen : public Rook, public Bishop
 {
-	/// Class queen shares one array for storing moves with Rook  and Bishop constructor
-	
+	//Class queen shares one array for storing moves with Rook and Bishop constructor
 public:
 	Queen() = delete;
-	Queen& operator =(Queen&) = delete;
+	Queen& operator = (Queen&) = delete;
 	Queen(Queen&) = delete;
+	virtual ~Queen();
 
 	Queen(Position* position, Color color, DynamicArray<Figure*>* takenFigures);
-	void getPossibleMoves( DynamicArray<Move*>* result)  override;
-	virtual ~Queen();
+	void getPossibleMoves(DynamicArray<Move*>* result) override;
 };
 
