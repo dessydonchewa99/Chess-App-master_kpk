69b33e74825897e7c319765f081add7780361ad6
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-08 00:04:30 +0300

    Optimize comment

--- ChessGamePracticumPROJECT/Queen.cpp
+++ ChessGamePracticumPROJECT/Queen.cpp
@@ -4,13 +4,12 @@
 #include"Rook.h"
 #include"Figure.h"
 
-Queen::Queen(Position * position, Color color, DynamicArray<Figure*>* takenFigures) : Figure(position, color, takenFigures),
-Rook(position, color, takenFigures), Bishop(position, color, takenFigures) {
+Queen::Queen(Position * position, Color color, DynamicArray<Figure*>* takenFigures) : Figure(position, color, takenFigures), Rook(position, color, takenFigures), Bishop(position, color, takenFigures) {
 	
 	this->setName("Queen");
 }
 void Queen::getPossibleMoves(DynamicArray<Move*>* result) {
-	// This it why it's enought to call only one of the getPossibleMoves method (Polymorphic)
+	// It's enought to call getPossibleMoves method (Polymorphic)
 	 Rook::getPossibleMoves(result);
 }
 
