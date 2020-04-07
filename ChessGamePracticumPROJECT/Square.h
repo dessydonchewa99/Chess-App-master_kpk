38b0c42612afcbf128de4959f17b50127a8c3972
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-07 23:50:22 +0300

    Following same structure as other files. isEmpty method and paramether change.

--- ChessGamePracticumPROJECT/Square.h
+++ ChessGamePracticumPROJECT/Square.h
@@ -1,18 +1,21 @@
 #pragma once
 #include"Figure.h"
-class Square
-{
+
+class Square {
+private: 
 	Figure* figure;
-	bool _isEmpty;
+	bool isEmpty;
 public:
-	
-	Square& operator =(Square&) = delete;
-	Square(Square&) = delete;
-	bool isEmpty() const;
-	void setFigure(Figure* figure);
-	Figure* getFigure()const;
-	Figure* removeFigure();
 	Square();
-	 ~Square();
+	Square& operator = (Square&) = delete;
+	Square(Square&) = delete;
+	~Square();
+
+	void setFigure(Figure* otherFigure);
+
+	bool getIsEmpty() const;
+	Figure* getFigure() const;
+
+	Figure* removeFigure();
 };
 
