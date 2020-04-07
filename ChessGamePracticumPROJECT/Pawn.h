0d73e5930eebf31ae4ed6854bfb64242c1bf901c
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-07 23:55:21 +0300

    Follow same structure as in other files

--- ChessGamePracticumPROJECT/Pawn.h
+++ ChessGamePracticumPROJECT/Pawn.h
@@ -1,19 +1,21 @@
 #pragma once
-#include "Figure.h"
+#include"Figure.h"
 #include"Board.h"
+
 #include<iostream>
-class Pawn :
-	public Figure
-{
+
+class Pawn : public Figure {
 public:
 	Pawn() = delete;
-	Pawn& operator =(Pawn&) = delete;
+	Pawn& operator = (Pawn&) = delete;
 	Pawn(Pawn&) = delete;
 	Pawn(Position* position, Color color, DynamicArray<Figure*>* takenFigures);
-	void getPossibleMoves( DynamicArray<Move*>* result)  override;
+
+	void getPossibleMoves(DynamicArray<Move*>* result) override;
 	void evolveToQueen() {
 		throw std::exception("Not implemented");
 	}
+
 	virtual ~Pawn();
 };
 
