18332a9df76c9105904e7cc729e22c6ea1edc14d
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-09 01:29:35 +0300

    Follow same structure as in other files. Parammeter in functions add

--- ChessGamePracticumPROJECT/View.h
+++ ChessGamePracticumPROJECT/View.h
@@ -1,16 +1,19 @@
 #pragma once
-#pragma once
+
 #include"IBoard.h"
 #include"GlobalVariables.h"
 #include"IView.h"
-class View :public  IView
-{
+
+class View : public View {
 public:
 	View();
+
 	virtual void printBoard(IBoard* board) = 0;
-	virtual void printMessageNL(const char*) = 0;
-	virtual void readCommand(char * command) = 0;
+	virtual void printMessageNL(const char* message) = 0;
+	virtual void readCommand(char* command) = 0;
+
 	virtual std::ostream& getStream() = 0;
+
 	virtual ~View();
 };
 
