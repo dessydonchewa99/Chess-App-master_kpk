9a64d36b1bce213fbe99a60b5a51108fa73c09e6
dessydonchewa99 <43968785+dessydonchewa99@users.noreply.github.com>
2020-04-09 01:31:34 +0300

    Follow same structure as in other files. Add comments in Bulgarian to think

--- ChessGamePracticumPROJECT/IView.h
+++ ChessGamePracticumPROJECT/IView.h
@@ -1,17 +1,14 @@
 #pragma once
+
 #include"IBoard.h"
 #include<iostream>
 
-class IView
-{
-
+//файлове и класове с I не е добра идея, какво е това I
+class IView {
 public:
-	
 	virtual void printBoard(IBoard* board) = 0;
-	virtual void printMessageNL(const char*) = 0;
+	virtual void printMessageNL(const char* message) = 0;
 	virtual std::ostream& getStream() = 0;
-	virtual void readCommand(char * command) = 0;
-	
-	
+	virtual void readCommand(char* command) = 0;	
 };
 
--- ChessGamePracticumPROJECT/View.cpp
+++ ChessGamePracticumPROJECT/View.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 #include "View.h"
 
-
-View::View()
-{
+View::View() {
+	std::cout<<"Default constructor of View class is called";
+	//не е качествен код, но празни също не е окей
 }
 
 
-View::~View()
-{
+View::~View() {
+	std::cout<<"Desturtor of View class is called";
 }
